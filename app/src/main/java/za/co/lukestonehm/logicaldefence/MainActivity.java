ad6816e0218f8179cd31ff9369c8eac71f66a545
Luke Johnstone <john.wella@gmail.com>
2015-01-17 23:44:32 +0200

    CHANGED:   #NavigationDrawerFragment removed in favor of more simple RelLayout            #ActionBar converted to ToolBar (support lib)            #spacing of nav drawer

--- app/src/main/java/za/co/lukestonehm/logicaldefence/MainActivity.java
+++ app/src/main/java/za/co/lukestonehm/logicaldefence/MainActivity.java
@@ -4,88 +4,135 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+        implements NavCallback {
 
-    /**
-     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
-     */
-    private NavigationDrawerFragment mNavigationDrawerFragment;
 
-    /**
-     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
-     */
+    public static final String TAG = "LogicalDefence";
+
     private CharSequence mTitle;
+    private String[] sections;
+
+    private int mCurrentSelectedPosition = 0;
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private ListView mDrawerListView;
+
+    NavCallback mCallbacks;
+    Menu currentActionViewMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
-
-//        Toolbar mActionBar = (Toolbar) findViewById(R.id.action_bar);
-//        setSupportActionBar(mActionBar);
-
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
+        drawScreen();
     }
 
-    @Override
-    public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
+    private void drawScreen() {
+        setContentView(R.layout.activity_main);
+        mCallbacks = this;
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+        sections = getResources().getStringArray(R.array.sections);
+
+        Toolbar mActionBar = (Toolbar) findViewById(R.id.action_bar);
+        setSupportActionBar(mActionBar);
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                    /* host Activity */
+                mDrawerLayout,                    /* DrawerLayout object */
+                R.string.navigation_drawer_open,  /* "open drawer" description for accessibility */
+                R.string.navigation_drawer_close  /* "close drawer" description for accessibility */
+        ) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+        mDrawerListView = (ListView) findViewById(R.id.lv);
+        mDrawerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                ListView lv = (ListView)view.getParent();
+                view.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                selectItem(position);
+            }
+        });
+
+        mDrawerListView.setAdapter(new ArrayAdapter<>(
+                this,
+                R.layout.nav_item,
+                R.id.nav_item, sections
+        ));
+
+        //TODO: Pref this
+        mTitle = sections[mCurrentSelectedPosition];
+        setTitle(mTitle);
+        mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
+        changeSection(mCurrentSelectedPosition);
+    }
+
+    private void selectItem(int position) {
+        mCurrentSelectedPosition = position;
+
+        mDrawerListView.setItemChecked(position, true);
+        mDrawerLayout.closeDrawers();
+        mCallbacks.onNavigationDrawerItemSelected(position);
+
+        mTitle = sections[position];
+        setTitle(mTitle);
+    }
+
+    private void changeSection(int position) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
                 .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
                 .commit();
     }
 
+    @Override
+    public void onNavigationDrawerItemSelected(int position) {
+        // update the main content by replacing fragments
+        changeSection(position);
+    }
+
     public void onSectionAttached(int number) {
-        switch (number) {
-            case 1:
-                mTitle = getString(R.string.title_section1);
-                break;
-            case 2:
-                mTitle = getString(R.string.title_section2);
-                break;
-            case 3:
-                mTitle = getString(R.string.title_section3);
-                break;
-        }
+        mTitle = sections[number - 1];
     }
-
-    public void restoreActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayShowTitleEnabled(true);
-        actionBar.setTitle(mTitle);
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
-            // Only show items in the action bar relevant to this screen
-            // if the drawer is not showing. Otherwise, let the drawer
-            // decide what to show in the action bar.
-            getMenuInflater().inflate(R.menu.main, menu);
-            restoreActionBar();
-            return true;
-        }
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        currentActionViewMenu = menu;
+        return true;
     }
 
     @Override
@@ -93,6 +140,10 @@ public class MainActivity extends ActionBarActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        Log.d(TAG, "TEST");
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -131,7 +182,11 @@ public class MainActivity extends ActionBarActivity
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_main, container, false);
+            View v = inflater.inflate(R.layout.fragment_main, container, false);
+            TextView tv = (TextView) v.findViewById(R.id.section_label);
+            tv.setText("Section: " + getArguments().getInt(ARG_SECTION_NUMBER));
+            //TODO: display things here
+            return v;
         }
 
         @Override
---
+++ app/src/main/java/za/co/lukestonehm/logicaldefence/NavCallback.java
@@ -0,0 +1,11 @@
+package za.co.lukestonehm.logicaldefence;
+
+/**
+ * Callbacks interface that all activities using this fragment must implement.
+ */
+public interface NavCallback {
+    /**
+     * Called when an item in the navigation drawer is selected.
+     */
+    void onNavigationDrawerItemSelected(int position);
+}
