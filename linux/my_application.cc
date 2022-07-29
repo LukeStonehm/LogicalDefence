982d7a07df8df821a3c01f5595863671c87bb78a
Luke Johnstone <luke@touchfoundry.co.za>
2022-07-29 11:59:21 +0200

    * set the app name across various supported platforms

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "logical_defence");
+    gtk_header_bar_set_title(header_bar, "Logical Defence");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "logical_defence");
+    gtk_window_set_title(window, "Logical Defence");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
