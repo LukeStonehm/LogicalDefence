4b99aec66572abaa69f49a3783dcbebf5ed2721e
Luke Johnstone <luke@touchfoundry.co.za>
2022-07-29 11:56:59 +0200

    * fixed test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,7 +14,7 @@ import 'package:logical_defence/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp(locale: Get.deviceLocale,));
+    await tester.pumpWidget(MyApp(locale: Get.deviceLocale, theme: ThemeMode.system,));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
